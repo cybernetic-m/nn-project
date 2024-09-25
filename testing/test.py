98b64b4fb1bb539463c481df7ce324a6aed09741
Massimo Romano <romano.massimo.uni@gmail.com>
2024-09-25 14:20:25 +0200

    loading models

--- testing/test.py
+++ testing/test.py
@@ -1,9 +1,14 @@
 import torch
 
-def testing(model, test_loader, test_metrics):
+def testing(model, model_path, test_loader, test_metrics):
 
     # Set the model in evaluation mode
     model.eval_mode()
+    
+    # Load the weights
+    model.load(model_path)
+
+    
 
     for data in test_loader:
         x, y_true = test_loader 
