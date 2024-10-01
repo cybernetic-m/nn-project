8295d349fcbce7ecafc0d863ec493f4806863d54
Massimo Romano <romano.massimo.uni@gmail.com>
2024-10-01 16:06:14 +0200

    other extractor :) I hope the last

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -12,26 +12,41 @@ class EMOVO_Dataset(Dataset):
     self.classes = ['dis', 'gio', 'neu', 'pau', 'rab', 'sor', 'tri']
     self.data = [] # Waw Audio as tensors
     self.labels = [] # Labels of the data
+    
+    extractor = feature_extractor() 
 
     file_list = os.listdir(dataset_dir)
     for filename in file_list:
       path = os.path.join(dataset_dir, filename)
       tensor, sample_rate = torchaudio.load(path)
+
       if tensor.shape[0] == 1: # if an audio is mono we duplicate the channel to make it stereo 
         tensor = torch.cat((tensor,tensor))
         print("fixed a mono track:",filename)
         torchaudio.save(path, tensor, sample_rate)
-      class_ = self.classes.index(filename.split('-')[0])
-      self.data.append((tensor,sample_rate))
-      self.labels.append(torch.tensor(class_))
-    self.device = device
 
+      if feature_extract:
+        features = extractor(tensor)
+        class_ = self.classes.index(filename.split('-')[0])
+        self.data.append((features,sample_rate))
+        print(features)
+        self.labels.append(torch.tensor(class_))
+
+      else:
+        class_ = self.classes.index(filename.split('-')[0])
+        self.data.append((tensor,sample_rate))
+        self.labels.append(torch.tensor(class_))
+
+    self.device = device
+    
+    '''
     if feature_extract:
       feature_extracto = feature_extractor(self)
       feature_extracto.apply()
       feature = feature_extracto.get_features()
       for i in range(len(self.data)):
-        self.data[i] = (feature[i].unsqueeze(0), self.data[i][1])  
+        self.data[i] = (feature[i].unsqueeze(0), self.data[i][1])
+    ''' 
 
   def __len__(self):
     return len(self.data)
--- dataloader/feature_extractor_tim.py
+++ dataloader/feature_extractor_tim.py
@@ -2,42 +2,34 @@ import torch
 import torchaudio.transforms as T
 
 class feature_extractor():
-    def __init__(self, dataset, frame_length = 2400, frame_shift = 600):
-        self.dataset = dataset
-        self.frame_length = frame_length
-        self.frame_shift = frame_shift
-        self.features = []
+    def __init__(self, sample_rate = 22050, frame_length_ms = 50, frame_shift_ms = 12.5, n_ftt=2048, n_mfcc=39):
+       
+        # Convert frame length and shift from milliseconds to samples
+        frame_length = int(sample_rate * (frame_length_ms / 1000))  # 50 ms to samples
+        frame_shift = int(sample_rate * (frame_shift_ms / 1000))  # 12.5 ms to samples
 
-    def apply(self):
-
-        for data, _ in self.dataset:
-            waveform, sample_rate = data
-            waveform = waveform.cpu()
-
-            mfcc = self.mfcc(waveform, sample_rate, 39)
-            # Average features across channels
-            mfcc_mean = mfcc.mean(dim=0).mean(dim=1)  # Average across channels
-
-            self.features.append(mfcc_mean)
-        
-    def get_features(self):
-        return self.features
-
-    def mfcc(self, waveform, sample_rate, n_mfcc):
-        n_fft = 2048
-        win_length = min(n_fft, self.frame_length)
-        mfcc_transform = T.MFCC(
-            sample_rate=sample_rate,
-            n_mfcc=n_mfcc,
+        self.mfcc_transform = T.MFCC(
+            sample_rate=sample_rate, # Sampling rate (22.05 kHz)
+            n_mfcc=n_mfcc, # Number of MFCCs to extract
             melkwargs={
-                "n_fft": n_fft,
-                "n_mels": 128,
-                "hop_length": self.frame_shift,
-                "win_length": win_length,
-                "window_fn": torch.hamming_window
+                "n_fft": n_ftt, # FFT points
+                "n_mels": 128, # Number of Mel Filter Banks
+                "hop_length": frame_shift, # Shift in samples
+                "win_length": frame_length, # Length in samples
+                "window_fn": torch.hamming_window # Hamming Window
             }
         )
-        mfcc = mfcc_transform(waveform)
+
+    def apply(self, waveform):
+
+        waveform = waveform.cpu()
+
+        mfcc = self.mfcc_transform(waveform)
+        # Average features across channels
+        #mfcc_mean = mfcc.mean(dim=0).mean(dim=1)  # Average across channels
+
+        print(mfcc.shape)
+
         return mfcc
     
    
