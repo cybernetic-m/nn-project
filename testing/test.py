07669814da81715c9e651aa14de9416db0f1bc9b
Massimo Romano <romano.massimo.uni@gmail.com>
2024-10-08 12:16:40 +0200

    elimination KAN results, update papers, inference time count

--- testing/test.py
+++ testing/test.py
@@ -2,6 +2,7 @@ import os
 import sys
 import torch
 import sklearn.metrics
+import time
 
 # Get the absolute paths of the directories containing the utils functions and testing directory
 utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils'))
@@ -25,6 +26,7 @@ def test(model, model_path, test_dataloader, test_metrics_dict, loss_fn):
     # Lists of predictions and true labels
     y_pred_list = []
     y_true_list = []
+    inference_time_list = []
 
     with torch.no_grad():
         for i, data in enumerate(test_dataloader):
@@ -35,8 +37,17 @@ def test(model, model_path, test_dataloader, test_metrics_dict, loss_fn):
             # Take only the waveform for the input tuple (waveform, sample_rate)
             x = x[0]       # x[0] = tensor([[[waveform]]], [sample_rate])
             
+            # Counting the time for inference
+            # Start counter
+            start_time = time.time()
+
             # Make predictions (Tensor with 7 values of probability, 1 for each class)
             y_pred = model(x)  # y_pred = tensor([[0.1254, 0.1152, 0.1396, 0.1600, 0.1058, 0.1394, 0.2146]],device='mps:0')
+
+            # Stop counter
+            end_time = time.time()
+            inference_time = end_time - start_time
+            inference_time_list.append(inference_time)
             
             # Compute the loss
             loss_value = loss_fn(y_pred, y_true)
@@ -47,6 +58,8 @@ def test(model, model_path, test_dataloader, test_metrics_dict, loss_fn):
             y_true_list += y_true.tolist()  
             # Argmax take the index (position) of the maximum value in y_pred => torch.argmax(y_pred) = tensor(6, device='mps:0') and item() take only the value 6
             y_pred_list += torch.argmax(y_pred.detach(), dim=1).cpu().tolist()
+            
+
     # Average Loss in testing
     loss_avg = loss / len(test_dataloader)   # tensor(value, device = 'cuda:0')
 
@@ -60,7 +73,7 @@ def test(model, model_path, test_dataloader, test_metrics_dict, loss_fn):
    
     save_metrics(test_metrics_dict, results_path + name_test_metrics)
 
-    return cm
+    return cm, inference_time_list
 
 
 
